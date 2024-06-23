70e6a4ff11c864c14238b2c27ac81db04cc2ed4f
Manuel-Lehrer <manuel.lehrer.official@gmail.com>
2024-06-23 15:47:04 +0700

    bugfix in split blocks function

--- src/block_markdown.py
+++ src/block_markdown.py
@@ -1,4 +1,9 @@
 def markdown_to_blocks(markdown):
-    split_markdown = markdown.split("\n")
+    split_markdown = markdown.split("\n\n")
     split_markdown = [block.strip() for block in split_markdown if block.strip() != ""]
+    i = 0
+    for block in split_markdown:
+        block = "\n".join([" ".join(line.split()) for line in block.split("\n")])
+        split_markdown[i]=block
+        i+=1
     return split_markdown
--- src/test_block_markdown.py
+++ src/test_block_markdown.py
@@ -4,13 +4,27 @@ from block_markdown import markdown_to_blocks
 
 class TestMarkdownToHTML(unittest.TestCase):
     def test_splitting_blocks(self):
-        test_blocks = ['This is **bolded** paragraph', 'This is another paragraph with *italic* text and `code` here', 'This is the same paragraph on a new line', '* This is a list', '* with items']
-        markdown = "This is **bolded** paragraph\nThis is another paragraph with *italic* text and `code` here\nThis is the same paragraph on a new line\n* This is a list\n* with items"
+        test_blocks = [
+                    "This is **bolded** paragraph",
+                    "This is another paragraph with *italic* text and `code` here\nThis is the same paragraph on a new line",
+                    "* This is a list\n* with items",
+            ]
+        markdown = """
+                    This is **bolded** paragraph
+
+                    This is another paragraph with *italic* text and `code` here
+                    This is the same paragraph on a new line
+
+                    * This is a list
+                    * with items
+                    """
         blocks = markdown_to_blocks(markdown)
         self.assertEqual(test_blocks, blocks)
 
     def test_splitting_blocks_newlines(self):
-        test_blocks = ['This is **bolded** paragraph', 'This is another paragraph with *italic* text and `code` here', 'This is the same paragraph on a new line', '* This is a list', '* with items']
+        test_blocks = ["This is **bolded** paragraph",
+                       "This is another paragraph with *italic* text and `code` here\nThis is the same paragraph on a new line",
+                       "* This is a list\n* with items",]
         markdown = """
                     This is **bolded** paragraph
 
